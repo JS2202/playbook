58888dce582b36d8fae3b01d933ae9c2472741f9
JS2202 <60891410+JS2202@users.noreply.github.com>
2022-04-22 23:43:35 -0500

    Weekly mission 1 completa

---
+++ weekly_mission_1/example10/pokemon.js
@@ -0,0 +1,11 @@
+export default class pokemon {
+    constructor(name) {
+        this.name = name;
+    }
+    sayHello() {
+        console.log(`Hola mi nombre es ${this.name}`);
+    }
+    sayMessage(message) {
+        console.log(`${this.name} dice: ${message}`);
+    }
+}
