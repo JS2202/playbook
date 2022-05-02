9df7aaf99435a4b47480c039ad7046352debdb1b
JS2202 <60891410+JS2202@users.noreply.github.com>
2022-05-01 19:25:59 -0500

    Algunos cambios

--- weekly_mission_3/03_twitter/test/views/userView.test.js
+++ weekly_mission_3/03_twitter/test/views/userView.test.js
@@ -10,7 +10,7 @@ describe('Tests for UserView', () => {
 
     test('Return an error object when try to create a new user with invalid properties', () => {
         const payload = { username: null, name: 12, id: "id" };
-        // const payload = { username: 'juliog', name: 'Julio Garcia', id: 1 };
+        // const payload = { username: 'js2202', name: 'Jose Armando Lopez Alvarez', id: 1 };
         const result = UserView.createUser(payload);
 
         expect(result.error).toMatch(/necesitan tener un valor valido/);
@@ -18,18 +18,18 @@ describe('Tests for UserView', () => {
 
     test('Return an error object when try to create a new user with missing properties', () => {
         const payload = { username: 'username' };
-        // const payload = { username: 'juliog', name: 'Julio Garcia', id: 1 };
+        // const payload = { username: 'js2202', name: 'Jose Armando Lopez Alvarez', id: 1 };
         const result = UserView.createUser(payload);
 
         expect(result.error).toMatch(/propiedades faltantes/);
     });
 
     test('Return an error object when try to create a new user with invalid properties', () => {
-        const payload = { username: 'juliog', name: 'Julio Garcia', id: 1 };
+        const payload = { username: 'js2202', name: 'Jose Armando Lopez Alvarez', id: 1 };
         const result = UserView.createUser(payload);
 
-        expect(result.username).toBe('juliog');
-        expect(result.name).toBe('Julio Garcia');
+        expect(result.username).toBe('js2202');
+        expect(result.name).toBe('Jose Armando Lopez Alvarez');
         expect(result.id).toBe(1);
     });
 });
